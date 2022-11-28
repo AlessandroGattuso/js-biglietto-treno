a8c79e991bd8446448ad8de4d08d2597d188d192
Alessandro <alessandrogattuso@yahoo.it>
2022-11-28 10:23:20 +0100

    added the message feature

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let price               // price of the ticket
 let km;                 // user's kilometers
 let age;                // user's age
+let message = "";             // message to display on screen
 
 //Asking the input values
 km = parseInt(prompt("Quanti chilometri vuoi percorrere?")); 
@@ -11,17 +12,20 @@ age = parseInt(prompt("Quanti anni hai"));
 price = km * 0.21;
 
 //Checking the age user, and in case, apply the discount 
-if(age < 18)                    //under 18
+if(age < 18){                    //under 18
   price = (price * 20) / 100;
-
-if(age > 65)                    //over 65
+  message = " Under 18";
+}
+if(age > 65){                    //over 65
   price = (price * 40) / 100;  
+  message = " Over 65";
+}
 
 //Floor the price  by 2 decimals(but it change the type of price(number => String))
 price = price.toFixed(2);
 
 //Print the final price
-console.log(price + '€');             
+console.log("Costo"+ message + ": "  + price + '€');             
   
 
 
